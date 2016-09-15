400828babd62f2e02beae4478264fc9edf06d52b
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-09-14 21:25:36 -0400

    add game move relations

--- db/migrate/20160913215835_create_moves.rb
+++ db/migrate/20160913215835_create_moves.rb
@@ -1,8 +1,8 @@
 class CreateMoves < ActiveRecord::Migration[5.0]
   def change
     create_table :moves do |t|
-
-      t.timestamps
+      t.column :move, :integer
+      t.column :created_at, :datetime
     end
   end
 end
