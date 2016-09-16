9567ec992c091e58a54b982c2720bb424960c447
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-09-16 11:42:17 -0400

    added belogns to

--- db/migrate/20160913215835_create_moves.rb
+++ db/migrate/20160913215835_create_moves.rb
@@ -1,6 +1,7 @@
 class CreateMoves < ActiveRecord::Migration[5.0]
   def change
     create_table :moves do |t|
+      t.belongs_to :game, index: true
       t.column :move, :integer
       t.column :created_at, :datetime
     end
