1c5da0ce8a4b2e9ed8802089e55d4f73af073147
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-09-16 14:20:53 -0400

    fixed missing ends for ifs

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   def init_game(x, y)
-    if !valid_square(x, y)
+    if !valid_square?(x, y)
       puts "Invalid square!"
     end
 
@@ -12,7 +12,7 @@ class Game < ApplicationRecord
   end
 
   def play(x, y)
-    if !valid_square(x, y)
+    if !valid_square?(x, y)
       puts "Invalid square!"
     end
 
@@ -27,7 +27,7 @@ class Game < ApplicationRecord
     game.save
   end
 
-  def get_board()
+  def get_board
     board = Array.new(3){Array.new(3, " ")}
     game = Game.find_by(id: 1)
 
@@ -47,29 +47,37 @@ class Game < ApplicationRecord
     # Three horizontal
     board.each do |row|
       next if row[0] == " "
-      if row.uniq.length == 1 then return row.uniq
+      if row.uniq.length == 1 then return row.uniq end
     end
 
     # Three vertical
     board.tranpose.each do |col|
       next if col[0] == " "
-      if col.uniq.length == 1 then return col.uniq
+      if col.uniq.length == 1 then return col.uniq end
     end
 
     # Three diagonal
     if board[0][0] != " " and 
        board[0][0] == board[1][1] and 
-       board[1][1] == board[2][2] then return board[0][0]
+       board[1][1] == board[2][2] then
+       return board[0][0]
+    end
 
     if board[2][0] != " " and
        board[2][0] == board[1][1] and
-       board[1][1] == board[0][2] then return board[2][0]
+       board[1][1] == board[0][2] then
+       return board[2][0]
+     end
 
     return nil
   end
 
   private
-    def valid_square(x, y)
-      0 < x and x < 2 and 0 < y and y < 2
+    def valid_square?(x, y)
+      if 0 < x and x < 2 and 0 < y and y < 2
+        return true
+      else 
+        return false
+      end
     end
 end
