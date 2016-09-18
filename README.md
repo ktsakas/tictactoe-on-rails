c8cb86e443f0813ec90bbd701604acbaaa07dc15
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-09-17 21:03:36 -0400

    removed csrf token verification

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # README
 
 TODO:
+* Use UUID's instead of increment ID
 * Move tests to fixtures
 * Add sockets instead of pulling
 * Allow for user login
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
     game = Game.find_by(id: params[:id])
 
@@ -7,7 +9,8 @@ class GamesController < ApplicationController
 
   def create
     game = Game.create({})
+    game.save
 
-
+    render json: game.to_json(:include => :moves)
   end
 end
