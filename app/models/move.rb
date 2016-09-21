e1a266baff44f8f690cd8b8fa5505d0ee24aeadd
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-09-20 22:24:27 -0400

    use built in validation methods

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,22 +1,20 @@
 class Move < ApplicationRecord
   belongs_to :game
+
+  validates :x,
+    inclusion: { in: 0..2, message: "must be between 0 and 2" },
+    presence: true
+
+  validates :y,
+    inclusion: { in: 0..2, message: "must be between 0 and 2" },
+    presence: true
+
   validate :game_is_not_finished
-  validate :move_is_valid
 
   # Make moves immutable
   def readonly?
     !new_record?
   end
-
-  def move_is_valid
-      if self.x < 0 or 2 < self.x
-        errors.add(:x, "invalid move x coordinate")
-      end
-
-      if self.y < 0 or 2 < self.y 
-        errors.add(:y, "invalid move y coordinate")
-      end
-    end
 
   def game_is_not_finished
     if self.game.is_finished?
